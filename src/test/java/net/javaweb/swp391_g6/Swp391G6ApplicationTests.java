0d4559fdf0db16f7e200c4bc50e5565bba9984ac
Le Manh <59783805+manh-vsc@users.noreply.github.com>
2024-05-12 17:35:54 +0700

    homepage layout

---
+++ src/test/java/net/javaweb/swp391_g6/Swp391G6ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaweb.swp391_g6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swp391G6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
