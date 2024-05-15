411342a69b7f65b68be716796f9f5c0a500718a8
Ducthanh2401 <119226851+Ducthanh2401@users.noreply.github.com>
2024-05-15 14:00:56 +0700

    demo2

---
+++ SWP391_6-demo1/src/main/java/net/javaweb/swp391_g6/ServletInitializer.java
@@ -0,0 +1,13 @@
+package net.javaweb.swp391_g6;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Swp391G6Application.class);
+	}
+
+}
---
+++ SWP391_6-demo1/src/main/java/net/javaweb/swp391_g6/Swp391G6Application.java
@@ -0,0 +1,13 @@
+package net.javaweb.swp391_g6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swp391G6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swp391G6Application.class, args);
+	}
+
+}
