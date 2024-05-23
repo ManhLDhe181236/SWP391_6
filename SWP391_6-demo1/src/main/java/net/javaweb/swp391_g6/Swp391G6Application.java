3335ffb7c7f4287ab4b34c8f8a121a21b10766ea
hiepdev3 <169441533+warrendevf@users.noreply.github.com>
2024-05-23 08:57:03 +0700

    ewq
    ds

--- SWP391_6-demo1/src/main/java/net/javaweb/swp391_g6/Swp391G6Application.java
+++ SWP391_6-demo1/src/main/java/net/javaweb/swp391_g6/Swp391G6Application.java
@@ -1,13 +1,39 @@
 package net.javaweb.swp391_g6;
 
+import io.github.cdimascio.dotenv.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class Swp391G6Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Swp391G6Application.class, args);
+		Dotenv dotenv = Dotenv.load();
+
+        String url = "jdbc:sqlserver://localhost:1433;databaseName=ExamDB";
+        String username = "sa";
+        String password = "123456";
+        // Thiết lập các biến môi trường trong hệ thống
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+        // Khởi động ứng dụng Spring Boot
+        SpringApplication.run(Swp391G6Application.class, args);
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            if (connection != null) {
+                System.out.println("Kết nối thành công: true");
+            } else {
+                System.out.println("Kết nối không thành công: false");
+            }
+        } catch (SQLException e) {
+            System.out.println("Kết nối không thành công: false");
+            e.printStackTrace();
+        }
 	}
 
 }
